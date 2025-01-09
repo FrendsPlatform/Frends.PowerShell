cb7633f4a9c39fdc0b924171786df42dd54df587
mateusz.wojtania <mateusz.wojtania@frends.com>
2025-01-09 07:48:09 +0100

    fix linter

--- Frends.PowerShell.RunCommand/Frends.PowerShell.RunCommand/PowerShell.cs
+++ Frends.PowerShell.RunCommand/Frends.PowerShell.RunCommand/PowerShell.cs
@@ -66,7 +66,7 @@ public static class PowerShell
     {
         List<dynamic> results = new();
         List<string> errors = new();
-        
+
         using Process process = new();
         process.StartInfo = new()
         {
